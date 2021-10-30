68be669c06ad3e23dfc6c3642c98b153636ac0ea
Daffa Shidqi <90913948+daffashid@users.noreply.github.com>
2021-10-30 19:04:12 +0700

    Add files via upload

---
+++ miniproject/script.js
@@ -0,0 +1,35 @@
+function feedback(){
+    document.getElementById("feeback").onclick=()=>{
+        alert("Thank you for filling in the feedback");
+    }
+}
+
+function modal(){
+    document.getElementById("modall").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("modalll").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("modalla").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("modala").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("modal").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("moda").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("mod").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("mo").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+    document.getElementById("m").onclick=()=>{
+        alert("the system is still not working properly, we apologize for the disturbance")
+    }
+}
